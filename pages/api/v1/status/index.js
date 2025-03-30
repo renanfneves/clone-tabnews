9cab18a3a3ea5945b8d49a4e6b102f02f9775045
Renan Floriano Neves <renanfneves@gmail.com>
2025-03-30 02:44:33 +0100

    add services scripts

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,9 @@
-import database from 'infra/database';
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query({
-    text: 'SELECT 1 + 1 as sum;',
-  });
+  const result = await database.query("SELECT 1 + 1 as sum");
   console.log(result.rows);
-  response.status(200).json({ status: 'OK' });
+  response.status(200).json({ chave: "são acima da média" });
 }
 
 export default status;
