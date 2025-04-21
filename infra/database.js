ff67a65b76a9eb86cccd94e42ff1fbd8f4d1c2eb
Renan Floriano Neves <renanfneves@gmail.com>
2025-04-21 16:29:07 +0100

    feat: add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,9 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
+    ssl: getSSLValues(),
   })
-  
+
   try {
     await client.connect()
     const result = await client.query(queryObject);
@@ -24,4 +24,14 @@ async function query(queryObject) {
 
 export default {
   query: query,
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
