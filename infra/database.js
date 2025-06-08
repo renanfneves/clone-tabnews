0e2770030d2c3ca0839deb3ab606bd06d801906e
Renan Floriano Neves <renanfneves@gmail.com>
2025-06-08 15:48:21 +0100

    feat: set controller pattern

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -7,9 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro dentro do catch do database.js:");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
@@ -24,6 +27,7 @@ async function getNewClient() {
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
+
   await client.connect();
   return client;
 }
@@ -32,6 +36,7 @@ const database = {
   query,
   getNewClient,
 };
+
 export default database;
 
 function getSSLValues() {
--- infra/errors.js
+++ infra/errors.js
@@ -1,17 +1,56 @@
+// extends Error porque o objeto Error padrão possui a stack trace do erro
 export class InternalServerError extends Error {
-  // herda de Error porque o objeto Error padrão possui a stack trace do erro
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Um erro interno não esperado aconteceu.", {
       cause,
     });
-    this.name = "InternalServerError"; // nome do erro para subscrever o nome padrão "Error"
+    this.name = "InternalServerError"; // nome do erro para subscrever o nome padrão "Error", este nome é exibido no console e em logs
     this.action = "Entre em contato com o suporte.";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    // retornamos propriedade por propriedade porque o JSON.stringify não serializa corretamente os objetos Error visto que eles possuem propriedades não enumeráveis
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Serviço indisponível no momento.", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Verifique se o serviço está disponível.";
+    this.statusCode = 503;
   }
 
   toJSON() {
     return {
-      // retornamos propriedade por propriedade porque o JSON.stringify não serializa corretamente os objetos Error visto que eles possuem propriedades não enumeráveis
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para este endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique se o método HTTP enviado é válido para este endpoint.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
       name: this.name,
       message: this.message,
       action: this.action,
