e7c77c297cf75c2e13b3b20323ec61bc2339b1f5
Renan Floriano Neves <renanfneves@gmail.com>
2025-06-15 15:00:22 +0100

    feat: faker + refactoring tests

--- models/password.js
+++ models/password.js
@@ -1,8 +1,10 @@
 import bcryptjs from "bcryptjs";
 
+const PEPPER = "your-secret-pepper"; // Replace with your actual pepper
+
 async function hash(password) {
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
+  return await bcryptjs.hash(`${password}_${PEPPER}`, rounds);
 }
 
 function getNumberOfRounds() {
@@ -10,7 +12,10 @@ function getNumberOfRounds() {
 }
 
 async function compare(providedPassword, storedPassword) {
-  return await bcryptjs.compare(providedPassword, storedPassword);
+  return await bcryptjs.compare(
+    `${providedPassword}_${PEPPER}`,
+    storedPassword,
+  );
 }
 
 const password = {
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -129,7 +129,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O username informado já está sendo utilizado.",
-        action: "Utilize outro email para realizar esta operação.",
+        action: "Utilize outro username para realizar esta operação.",
         status_code: 400,
       });
     });
